c46ff862824983b40247e99c85d843d66d348ed9
LouisAldorioEfendi <louisaldorio@gmail.com>
2021-08-18 22:31:40 +0700

    minor changes to proceed to pull request

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -26,6 +26,7 @@ class App {
             printNameShodiq()
             printNameRifki()
             printNameAlun()
+            printNameAlun()
         } else {
             println("Login Failed")
         }
